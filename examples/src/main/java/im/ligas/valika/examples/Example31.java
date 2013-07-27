1dd5d80401437288616f083fecce9100fea54d8e
valika <Princezna@valika>
2013-07-27 18:05:05 +0200

    example31

---
+++ examples/src/main/java/im/ligas/valika/examples/Example31.java
@@ -0,0 +1,16 @@
+package im.ligas.valika.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class Example31{
+    public static void main( String[] args ){
+        System.out.println("Natural count of 100: "+naturalCount(100));
+    }
+    
+    private static long naturalCount(long n){
+        //if(n>0 && )
+        return 0;
+    }
+}
