ec77006817f080153180ddc0e54c47ebe83e9fb2
valika <Princezna@valika>
2013-07-27 17:48:08 +0200

    new project added

---
+++ examples/src/main/java/im/ligas/valika/examples/App.java
@@ -0,0 +1,13 @@
+package im.ligas.valika.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
